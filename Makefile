b9a53beb052a44f81000a91ba85af5a06b428131
Kihong Heo <kihong.heo@kaist.ac.kr>
2020-06-17 17:20:19 +0900

    [Bingo] use linenoise

--- Makefile
+++ Makefile
@@ -115,8 +115,11 @@ utils : utils/createfg$(EE) utils/fg2dot$(EE) utils/fginfo$(EE) utils/uai2fg$(EE
 
 lib: $(LIB)/libdai$(LE)
 
-bingo: lib
-	$(CC) -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -fopenmp wrapper.cpp ./lib/libdai.a -o bingo -lgmp -lgmpxx
+linenoise:
+	clang -fpic -c linenoise.c
+
+bingo: lib linenoise
+	$(CC) -std=c++11 -O2 -march=native -Wall -Wextra -Werror -I ./include -fopenmp wrapper.cpp linenoise.o ./lib/libdai.a -o bingo -lgmp -lgmpxx
 
 # OBJECTS
 ##########
--- wrapper.cpp
+++ wrapper.cpp
@@ -10,6 +10,9 @@ using namespace dai;
 #include <map>
 #include <string>
 #include <vector>
+
+#include "linenoise.h"
+
 using namespace std;
 
 static FactorGraph fg;
@@ -27,52 +30,62 @@ void initBP() {
     bp.init();
 }
 
-void queryVariable() {
-    int varIndex;
-    cin >> varIndex;
-    clog << __LOGSTR__ << "Q " << varIndex << endl;
+void queryVariable(stringstream &SS) {
+  int varIndex;
+  SS >> varIndex;
+  clog << __LOGSTR__ << "Q " << varIndex << endl;
 
-    // auto ans = bp.belief(fg.var(varIndex)).get(1);
-    auto ans = bp.newBelief(varIndex);
-    clog << __LOGSTR__ << "Returning " << ans << "." << endl;
-    cout << ans << endl;
+  // auto ans = bp.belief(fg.var(varIndex)).get(1);
+  auto ans = bp.newBelief(varIndex);
+  clog << __LOGSTR__ << "Returning " << ans << "." << endl;
+  cout << ans << endl;
 }
 
-void queryFactor() {
-    int factorIndex, valueIndex;
-    cin >> factorIndex >> valueIndex;
-    clog << __LOGSTR__ << "FQ " << factorIndex << " " << valueIndex << endl;
+void queryFactor(stringstream &SS) {
+  int factorIndex, valueIndex;
+  SS >> factorIndex >> valueIndex;
+  clog << __LOGSTR__ << "FQ " << factorIndex << " " << valueIndex << endl;
 
-    auto ans = bp.beliefF(factorIndex).get(valueIndex);
-    clog << __LOGSTR__ << "Returning " << ans << "." << endl;
-    cout << ans << endl;
+  auto ans = bp.beliefF(factorIndex).get(valueIndex);
+  clog << __LOGSTR__ << "Returning " << ans << "." << endl;
+  cout << ans << endl;
 }
 
-void runBP() {
-    double tolerance;
-    size_t minIters, maxIters, histLength;
-    cin >> tolerance >> minIters >> maxIters >> histLength;
-    clog << __LOGSTR__ << "BP " << tolerance << " " << minIters << " " << maxIters << " " << histLength << endl;
+void runBP(stringstream &SS) {
+  double tolerance;
+  size_t minIters, maxIters, histLength;
+  SS >> tolerance >> minIters >> maxIters >> histLength;
+  clog << __LOGSTR__ << "BP " << tolerance << " " << minIters << " " << maxIters
+       << " " << histLength << endl;
 
-    double yetToConvergeFraction = bp.run(tolerance, minIters, maxIters, histLength);
-    cout << yetToConvergeFraction << endl;
+  double yetToConvergeFraction =
+      bp.run(tolerance, minIters, maxIters, histLength);
+  cout << yetToConvergeFraction << endl;
 }
 
-void clamp() {
-    int varIndex;
-    string varValueStr;
-    cin >> varIndex >> varValueStr;
-    assert(varValueStr == "true" || varValueStr == "false");
-    clog << __LOGSTR__ << "O " << varIndex << " " << varValueStr << endl;
-
-    bool varValue = (varValueStr == "true");
-    clamps[varIndex] = varValue;
-    initBP();
-    cout << "O " << varIndex << " " << varValueStr << endl;
+void clamp(stringstream &SS) {
+  int varIndex;
+  string varValueStr;
+  SS >> varIndex >> varValueStr;
+  clog << __LOGSTR__ << "O " << varIndex << " " << varValueStr << endl;
+  if (varValueStr.empty()) {
+    clog << "Invalid arguments" << endl;
+    return;
+  }
+  if (varValueStr != "true" && varValueStr != "false") {
+    clog << varIndex << "is " << varValueStr << " is an invalid boolean value"
+         << endl;
+    return;
+  }
+  bool varValue = (varValueStr == "true");
+  clamps[varIndex] = varValue;
+  initBP();
+  cout << "O " << varIndex << " " << varValueStr << endl;
 }
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
+        cerr << __LOGSTR__ << "Insufficient number of arguments." << endl;
         cerr << __LOGSTR__ << "Insufficient number of arguments." << endl;
         return 1;
     }
@@ -91,23 +104,31 @@ int main(int argc, char *argv[]) {
 
     initBP();
 
-    string cmdType;
-    while (cin >> cmdType) {
-        clog << __LOGSTR__ << "Read command " << cmdType << endl;
-        if (cmdType == "Q") {
-            queryVariable();
-        } else if (cmdType == "FQ") {
-            queryFactor();
-        } else if (cmdType == "BP") {
-            runBP();
-        } else if (cmdType == "O") {
-            clamp();
-        } else {
-            assert(cmdType == "NL");
-            cout << endl;
-        }
-    }
+    linenoiseHistoryLoad("history.txt");
+    char *line;
+    string CmdType;
+    while ((line = linenoise("bingo> ")) != NULL) {
 
+      linenoiseHistoryAdd(line);
+      linenoiseHistorySave("history.txt");
+      string Line(line);
+      stringstream SS(Line);
+      SS >> CmdType;
+      clog << __LOGSTR__ << "Read command " << CmdType << endl;
+      if (CmdType == "Q") {
+        queryVariable(SS);
+      } else if (CmdType == "FQ") {
+        queryFactor(SS);
+      } else if (CmdType == "BP") {
+        runBP(SS);
+      } else if (CmdType == "O") {
+        clamp(SS);
+      } else {
+        cout << "Invalid command" << endl;
+      }
+
+      free(line);
+    }
     clog << __LOGSTR__ << "Bye!" << endl;
     return 0;
 }
